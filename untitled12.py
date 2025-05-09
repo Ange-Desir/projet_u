5d25f9c75f2bc332b8fc553b10e05391b45c0427
Ange-Desir <madouange48@gmail.com>
2025-05-09 17:57:52 +0000

    Update untitled12.py

--- untitled12.py
+++ untitled12.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1ZuLF3do5--NZEXrCAIiI2N3sqLU6n46I
 """
 
-!pip install streamlit
 import streamlit as st
 import pandas as pd
 
